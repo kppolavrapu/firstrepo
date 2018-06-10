19fec7242c77910d41b3453179361f65265d94e7
Krishna Prasad <krishnaprasad.polavarapu@gmail.com>
2018-06-10 15:32:13 +0530

    class Demo python file added

---
+++ ClassDemo.py
@@ -0,0 +1,28 @@
+class Employee:
+
+    def __init__(self,first_name,last_name,salary):
+        self.first = first_name
+        self.last = last_name
+        self.sal= salary
+        self.email = first_name + '.' + last_name + '@company.com'
+
+
+    def fullname(self):
+        return '{} {}'.format(self.first,self.last)
+
+emp1 =  Employee('krishna','polavarapu',25000)
+print(emp1.email)
+        print(emp1.fullname())
+# print(Employee.fullname(emp1))
+
+# emp2 =  Employee()
+# emp1.first_name = 'krishna'
+# emp1.last_name = 'polavarapu'
+# emp1.salary =  40000
+
+# emp2.first_name = 'prasad'
+# emp2.last_name = 'polavarapu'
+# emp1.salary = 30000
+
+# print(emp1.salary)
+# print(emp2.last_name)
